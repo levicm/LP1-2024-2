8ba1cae62540fb029452a78531018f18fdf494a2
Levi Mota <levi.mota@gmail.com>
2024-12-13 21:44:05 -0300

    Aula de condicionais

---
+++ a07-condicionais/media2.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Informe a nota da unidade 1:"))
+nota2 = float(input("Informe a nota da unidade 2:"))
+media = (nota1 + nota2) / 2
+
+if media >= 6:
+    print("Aprovado!")
+else:
+    print("Reprovado!")
